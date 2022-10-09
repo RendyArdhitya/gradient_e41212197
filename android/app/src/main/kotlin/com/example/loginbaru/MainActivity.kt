72be4b166c752faa57188b1b84ccc10e0fbbb701
The_Acimalaka <96220717+ShikigamiKontoru@users.noreply.github.com>
2022-10-09 15:29:30 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/loginbaru/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.loginbaru
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final List<Page> _pages = [
+    Page('Home', Icons.home),
+    Page('Discover', Icons.search),
+    Page('Playlist', Icons.playlist_add),
+  ];
+
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentPageIndex = 0;
+
+  void _openPage(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> drawerItemWidgets = widget._pages
+        .asMap()
+        .map((int index, Page page) => MapEntry<int, Widget>(
+            index,
+            ListTile(
+              title: Text(page.title),
+              leading: Icon(page.iconData),
+              selected: _currentPageIndex == index,
+              onTap: () {
+                _openPage(index);
+                Navigator.pop(context);
+              },
+            )))
+        .values
+        .toList();
+    drawerItemWidgets.insert(
+      0,
+      DrawerHeader(
+        child: Text('Home'),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+        ),
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Music.io"),
+      ),
+      body: Center(
+        child: Text(widget._pages[_currentPageIndex].title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: drawerItemWidgets,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPageIndex,
+        items: widget._pages
+            .map((Page page) => BottomNavigationBarItem(
+                  icon: Icon(page.iconData),
+                  label: page.title,
+                ))
+            .toList(),
+        onTap: _openPage,
+      ),
+    );
+  }
+}
+
+class Page {
+  final String title;
+  final IconData iconData;
+  Page(this.title, this.iconData);
+}
